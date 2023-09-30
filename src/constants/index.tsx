d606559f3ee92b741a8ce950348181521ee66a05
Max Knivets <knivets.m@gmail.com>
2023-09-30 09:15:18 -0400

    chore: refactor constants

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,38 +1,19 @@
-import { GatingType, JoinContainerConfig, createContainerArgs } from "@/types";
+export const contract_id = "mint.sharddog.near";
+export const functionId = "IsQ2GbvmQjdcuMV0XEo2onp-Cld3Yk9uMC8n0HMArs0";
 
-// TODO get new contract
-export const functionId = "bICP3RVVlAxY1Tama2yiT2kXj1Z8kWJvJFV39oiadYE";
+export const defaultWallet = "my-near-wallet";
 
-export const userMessage = "sharddog-mem::";
 export const adminMessage = "sharddog-admin-mem::";
 
-export const emptyCreateContainerConfig: createContainerArgs = {
-  vault_id: "1",
-  gating_type: GatingType.ERC20,
-  token_address: "",
-  max_entries: 100,
-  token_threshold: 1,
-};
-
-export const emptyJoinContainerConfig: JoinContainerConfig = {
-  container_id: "",
-};
+export const ToastOptions = { duration: 3000 };
 
 export const MockMEMState = {
   evm_molecule_endpoint: "http://evm.molecule.sh",
-  admin_address: "0x197f818c1313dc58b32d88078ecdfb40ea822614",
-  users_counter: 0,
+  admin_address: "0x29942a1ab52ea0A7c2c7C9DFE637710Cb460F61F",
   admin_counter: 0,
   containers: [],
   signatures: [],
   publicFunctions: {
-    createContainer: ["config", "caller", "user_sig", "admin_sig"],
-    joinContainer: [
-      "caller",
-      "user_sig",
-      "admin_sig",
-      "container_id",
-      "vault_id",
-    ],
+    createContainer: ["admin_sig", "nft_id", "content"],
   },
 };
