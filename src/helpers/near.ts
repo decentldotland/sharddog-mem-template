0c821ae2bde15f0e0a772ca6098bb576406d1ff4
Max Knivets <knivets.m@gmail.com>
2023-09-30 09:16:59 -0400

    feat: more near methods

--- src/helpers/near.ts
+++ src/helpers/near.ts
@@ -3,3 +3,18 @@ export async function getAccounts(selector: any, walletName: string) {
   const accounts = await wallet.getAccounts();
   return accounts;
 }
+
+export async function connect(
+  selector: any,
+  walletName: string,
+  contractId: string
+) {
+  const wallet = await selector.wallet(walletName);
+  const accounts = await wallet.signIn({ contractId });
+  return accounts;
+}
+
+export async function disconnect(selector: any, walletName: string) {
+  const wallet = await selector.wallet(walletName);
+  await wallet.signOut();
+}
