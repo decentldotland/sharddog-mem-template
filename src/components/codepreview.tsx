304fa1ad61282423081d4a141ed119e50a39a1f0
Max Knivets <knivets.m@gmail.com>
2023-09-30 22:12:50 -0400

    fix: border

--- src/components/codepreview.tsx
+++ src/components/codepreview.tsx
@@ -8,8 +8,8 @@ interface CodePreviewProps {
 export default function CodePreview({ state, isMockup }: CodePreviewProps) {
   return (
     <>
-      <h2 className="font-bold text-center">Application state</h2>
-      <div className="border-2 border-black max-h-[400px] overflow-y-scroll p-2 max-w-[60vw]">
+      <h2 className="font-bold text-center">MEM Application state</h2>
+      <div className="border-2 border-black dark:border-white max-h-[400px] overflow-y-scroll p-2 max-w-[60vw]">
         <pre>
           <code>{JSON.stringify(state, null, 4)}</code>
         </pre>
