1f1d68df51bee281c0d5be03ec2fde86212430e6
Max Knivets <knivets.m@gmail.com>
2023-09-30 21:45:23 -0400

    fix: rename

--- src/pages/api/upload-and-encrypt.ts
+++ src/pages/api/upload-and-encrypt.ts
@@ -12,8 +12,9 @@ export default async function handler(
     if (!TXes) throw new Error("No TXes attached");
     const list = { files: TXes };
     const jsonStr = JSON.stringify(list, null, 2);
-    const bufferFile = Buffer.from(jsonStr);
-    const result = await uploadFileToArseedNode(bufferFile, "application/json");
+    const encrypted = await encryptEVMMessage(jsonStr);
+    const bufferFile = Buffer.from(encrypted);
+    const result = await uploadFileToArseedNode(bufferFile, "text/plain");
     const itemId = result?.order?.itemId || "";
     const encryptedTX = await encryptEVMMessage(itemId);
     return res.status(200).json(encryptedTX);
