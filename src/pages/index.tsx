6c481d13ad441d01fac322a9ce1612a116045f0b
Max Knivets <knivets.m@gmail.com>
2023-09-30 21:49:43 -0400

    feat: index page complete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,25 @@ import React, { useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
 import { WalletSelectorModal } from "@near-wallet-selector/modal-ui";
 
-import { contract_id, defaultWallet } from "@/constants";
+import {
+  MockMEMState,
+  ToastOptions,
+  contract_id,
+  defaultWallet,
+} from "@/constants";
+import { findByNFTId, readMEM } from "@/helpers";
 import { disconnect, getAccounts } from "@/helpers/near";
 import { decryptEVMMessageWeb } from "@/helpers/encryption";
+import { getWalletNFTs } from "@/helpers/indexer";
+import { downloadAndDecrypt } from "@/helpers/arseed";
+
+import { NFT } from "@/types/indexer";
+import { MEMState } from "@/types/state";
 
 import Guide from "@/components/guide";
 import nearModal from "@/components/wrapper";
 import Navbar from "@/components/navbar";
-import { getWalletNFTs } from "@/helpers/indexer";
-import { IndexerData, NFT } from "@/types/indexer";
+import FileLinks from "@/components/fileLinks";
 
 export default function Home() {
   // NEAR
@@ -19,26 +29,51 @@ export default function Home() {
   const [selector, setSelector] = useState<any>();
   const [NFTs, setNFTs] = useState<NFT[]>([]);
 
-  async function decrypt(hash: string) {
-    const decryptedTX = await decryptEVMMessageWeb(hash);
-    const Comp = () => (
-      <>
-        <div>Success!</div>{" "}
-        <a
-          href={`https://arseed.web3infra.dev/${decryptedTX}`}
-          className="underline"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Arseeding URL
-        </a>
-      </>
+  // state and handlers
+  const [state, setState] = useState<MEMState>();
+  const [stateInit, setStateInit] = useState<boolean>(true);
+
+  async function reveal(tokenId: string) {
+    if (!state) {
+      toast.error(
+        "Weird. How did this happen? Where is the contract state?",
+        ToastOptions
+      );
+      return;
+    }
+    const nft = findByNFTId(state, tokenId);
+    if (!nft) {
+      toast.error("No goodies for this NFT yet!", ToastOptions);
+      return;
+    }
+    const fileLinks = await downloadAndDecrypt(nft.content);
+    if (!fileLinks) {
+      toast.error("No goodies for this NFT yet!", ToastOptions);
+      return;
+    }
+    updateNftFileLinks(tokenId, fileLinks.files);
+    toast.success(
+      "Decrypted " + fileLinks.files.length + " files for tokenId " + tokenId,
+      ToastOptions
     );
-    toast.success(<Comp />, { duration: 10000 });
+  }
+
+  function updateNftFileLinks(tokenId: string, fileLinks: string[]) {
+    setNFTs((prevNfts) => {
+      return prevNfts.map((nft) => {
+        if (nft.nft.token_id === tokenId) {
+          return {
+            ...nft,
+            fileLinks,
+          };
+        }
+        return nft;
+      });
+    });
   }
 
   useEffect(() => {
-    const initNEAR = async () => {
+    async function initNEAR() {
       try {
         const { selector, modal } = await nearModal(contract_id);
         setSelector(selector);
@@ -46,14 +81,28 @@ export default function Home() {
         const accounts = (await getAccounts(selector, defaultWallet))[0];
         setNearAccounts(accounts);
         const indexer = await getWalletNFTs(accounts?.accountId, [contract_id]);
-        const { wallet_holdings_by_collection } = indexer.near;
-        const NFTs = wallet_holdings_by_collection;
+        const NFTs = indexer.near.wallet_holdings_by_collection;
         setNFTs(NFTs);
       } catch (e) {
         console.log(e);
       }
-    };
+    }
+    async function downloadState() {
+      // read and set MEM state object
+      let MEMState;
+      try {
+        MEMState = await readMEM();
+        setState(MEMState);
+        if (!MEMState) {
+          setState(MockMEMState);
+          setStateInit(false);
+        } else setStateInit(true);
+      } catch (e: any) {
+        console.log(e.message);
+      }
+    }
     initNEAR();
+    downloadState();
   }, []);
 
   return (
@@ -79,11 +128,27 @@ export default function Home() {
             </button>
           )}
         </div>
-        {NFTs.map((NFT) => (
-          <div className="flex flex-col items-center justify-center">
-            <img src={NFT.nft.media_url} className="w-40 h-40" />
-          </div>
-        ))}
+        <h2 className="text-2xl font-bold">Shard Dogs</h2>
+        <div className="grid grid-cols-3 gap-x-4">
+          {NFTs.map((NFT) => (
+            <div className="flex flex-col gap-y-2 items-center justify-center max-w-[192px] text-center">
+              <img src={NFT.nft.media_url} className="w-48 h-48" />
+              <div className="">
+                {NFT.nft.name} ({NFT.nft.token_id})
+              </div>
+              {NFT?.fileLinks?.length ? (
+                <FileLinks fileLinks={NFT.fileLinks} />
+              ) : (
+                <button
+                  onClick={() => reveal(NFT.nft.token_id)}
+                  className="border px-2 py-1"
+                >
+                  Reveal Goodies
+                </button>
+              )}
+            </div>
+          ))}
+        </div>
       </main>
     </>
   );
