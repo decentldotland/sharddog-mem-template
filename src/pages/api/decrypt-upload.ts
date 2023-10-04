ef2b33a3fbf1167d956451da44f96a94f791531c
Max Knivets <knivets.m@gmail.com>
2023-10-04 07:13:02 -0400

    feat: new decrypt API

--- src/pages/api/decrypt-upload.ts
+++ src/pages/api/decrypt-upload.ts
@@ -3,15 +3,20 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 import { arseedURL } from "@/constants";
 import { decryptEVMMessageNode } from "@/helpers/encryption";
+import { verifyDecrypt } from "@/helpers";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
   try {
-    const { hash } = req.body;
-    if (!hash) throw new Error("No hash to decrypt!");
-    const decryptedTX = await decryptEVMMessageNode(hash);
+    const { contentHash, nearHash } = req.body;
+    if (!contentHash || !nearHash) throw new Error("No hash to decrypt!");
+    const state = await verifyDecrypt(nearHash);
+    console.log("state", state);
+    if (!state?.decryption_hashes?.find((nearHash) => nearHash === nearHash))
+      console.log("shit?");
+    const decryptedTX = await decryptEVMMessageNode(contentHash);
     const fileHash = (await axios.get(arseedURL + decryptedTX)).data;
     const decryptedJson = JSON.parse(
       (await decryptEVMMessageNode(fileHash)) || ""
