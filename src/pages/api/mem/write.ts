9a590c82bb74a07a778f9f4d7e92f063ba99af8e
Max Knivets <knivets.m@gmail.com>
2023-10-04 07:12:50 -0400

    chore: refactor write API

--- src/pages/api/mem/write.ts
+++ src/pages/api/mem/write.ts
@@ -1,22 +1,13 @@
-import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
-const headers = {
-  headers: {
-    "Content-Type": "application/json",
-  },
-};
+import { uploadToMEM } from "@/helpers";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
   try {
-    const url = "https://api.mem.tech/api/transactions";
-    const request = (await axios.post(url, req.body, headers)).data;
-    const state = request?.data?.execution?.state;
-    const errors = request?.data?.execution?.errors;
-    console.log(request.data);
+    const { state, errors } = await uploadToMEM(req.body);
     return res.status(200).json({ state, errors });
   } catch (error: any) {
     console.log(error);
